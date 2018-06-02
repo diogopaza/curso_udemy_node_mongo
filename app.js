bc4ea1a5a28d233cc9be5273eb3ea6e0b4c80aa5
Diogo Paza <diogopazacvel@gmail.com>
2018-06-02 18:58:05 -0300

    reinicio curso

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express();
+
+app.set('view engine','ejs')
+
+app.get('/formulario_inclusao_noticia',function(req, res){
+    
+            res.render('admin/form_add_noticia')
+    })
+
+    app.get('/',function(req, res){
+        
+                res.render('home/index')
+        })
+
+app.get('/noticias',function(req, res){
+
+        res.render('noticias/noticias')
+})
+
+   
+
+app.listen(3000, function(){
+    console.log('escutando na 3000 com express')
+})
+
+
+
+
