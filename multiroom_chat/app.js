6268cd5a14822a866ddc63632500d1e881292e8f
Diogo Paza <diogopazacvel@gmail.com>
2018-07-08 18:30:05 -0300

    websocket

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -4,6 +4,17 @@ var app = require('./config/server')
 /*qual porta vai responder */
 
 
-app.listen(80,function(){
+var server = app.listen(80,function(){
     console.log('servidor online')
+})
+
+io = require('socket.io').listen(server);
+/*  criar a conexao por websocket*/
+
+io.on('connection',function(socket){
+    console.log('Usuario conectou')
+    socket.on('disconnect',function(){
+        console.log('descoonectou')
+    })
+    
 })
