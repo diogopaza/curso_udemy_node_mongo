31d47c24957d1e0a70c2c14c53260640e17b9141
Diogo Paza <diogopazacvel@gmail.com>
2018-08-09 21:53:12 -0300

    cont session

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,5 +1,22 @@
 module.exports.index = function(application, req, res){
     
-        res.render('index');
+        res.render('index', { validacao:{} });
     
     }
+
+    module.exports.autenticar = function(application, req, res){
+        
+           var dados = req.body
+           req.assert('usuario', 'usuario não pode estar vazio').notEmpty()
+           req.assert('senha', 'Senha não pode estar vazio').notEmpty()
+
+           var erros = req.validationErrors()
+           if(erros){
+              
+               res.render('index', {validacao:erros})
+               return
+           }
+
+           res.send('tudo ok para criar a sessao')
+        
+        }
